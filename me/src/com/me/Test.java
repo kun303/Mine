0b4b9f41a48b33e932084552bfbdbbf5a4781671
Sorry <kkunjia@gmail.com>
2014-05-03 21:11:36 +0800

    add develop

--- me/src/com/me/Test.java
+++ me/src/com/me/Test.java
@@ -2,8 +2,6 @@ package com.me;
 
 public class Test {
 public static void main(String[] args) {
-	System.out.println("hahahahahjjjjjjjjjjjjjjjjk");
-	
-	dfdssfdsfdfdsfd
+	System.out.println("haode");
 }
 }
